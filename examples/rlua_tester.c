a4f98d983b40d9a2d85b5d8033c251cf0babbc86
Ray <raysan5@gmail.com>
2017-05-09 11:22:12 +0200

    Update rlua_tester.c

--- examples/rlua_tester.c
+++ examples/rlua_tester.c
@@ -1,6 +1,6 @@
 /*******************************************************************************************
 *
-*   raylib [rlua] example - Lua file execution
+*   raylib [rlua] examples test suite
 *
 *   NOTE: This example requires Lua library (http://luabinaries.sourceforge.net/download.html)
 *
@@ -20,7 +20,7 @@
 #include "raylib.h"
 
 #define RLUA_IMPLEMENTATION
-#include "rlua.h"
+#include "rlua.h"               // raylib Lua binding
 
 int main()
 {
@@ -29,61 +29,80 @@ int main()
     InitLuaDevice();
     //--------------------------------------------------------------------------------------
 
-    // ExecuteLuaFile("core_basic_window.lua");                 // OK!
-    // ExecuteLuaFile("core_input_keys.lua");                   // OK!
-    // ExecuteLuaFile("core_input_mouse.lua");                  // OK!
-    // ExecuteLuaFile("core_mouse_wheel.lua");                  // OK!
-    // ExecuteLuaFile("core_input_gamepad.lua");                // OK!
-    // ExecuteLuaFile("core_random_values.lua");                // OK!
-    // ExecuteLuaFile("core_color_select.lua");                 // OK!
-    // ExecuteLuaFile("core_drop_files.lua");                   // OK!
-    // ExecuteLuaFile("core_storage_values.lua");               // OK!
-    // ExecuteLuaFile("core_gestures_detection.lua");           // OK!
-    // ExecuteLuaFile("core_3d_mode.lua");                      // OK!
-    // ExecuteLuaFile("core_3d_picking.lua");                   // OK!
-    // ExecuteLuaFile("core_3d_camera_free.lua");               // OK!
-    // ExecuteLuaFile("core_3d_camera_first_person.lua");       // OK!
-    // ExecuteLuaFile("core_2d_camera.lua");                    // OK!
-    // ExecuteLuaFile("core_world_screen.lua");                 // OK!
-    ExecuteLuaFile("core/core_vr_simulator.lua");                 
-    // ExecuteLuaFile("shapes_logo_raylib.lua");                // OK!
-    // ExecuteLuaFile("shapes_basic_shapes.lua");               // OK!
-    // ExecuteLuaFile("shapes_colors_palette.lua");             // OK!
-    // ExecuteLuaFile("shapes_logo_raylib_anim.lua");           // OK!
-    ExecuteLuaFile("shapes/shapes_lines_bezier.lua");
-    // ExecuteLuaFile("textures_logo_raylib.lua");              // OK!
-    // ExecuteLuaFile("textures_image_loading.lua");            // OK!
-    // ExecuteLuaFile("textures_rectangle.lua");                // OK!
-    // ExecuteLuaFile("textures_srcrec_dstrec.lua");            // OK!
-    // ExecuteLuaFile("textures_to_image.lua");                 // OK!
-    // ExecuteLuaFile("textures_raw_data.lua");                 // ERROR: LoadImageEx()
-    // ExecuteLuaFile("textures_formats_loading.lua");          // OK!
-    // ExecuteLuaFile("textures_particles_blending.lua");       // OK!
-    // ExecuteLuaFile("textures_image_processing.lua");         // ERROR: GetImageData() --> UpdateTexture()
-    // ExecuteLuaFile("textures_image_drawing.lua");            // OK!
-    // ExecuteLuaFile("text_sprite_fonts.lua");                 // OK!
-    // ExecuteLuaFile("text_bmfont_ttf.lua");                   // OK!
-    // ExecuteLuaFile("text_raylib_fonts.lua");                 // OK!
-    // ExecuteLuaFile("text_format_text.lua");                  // OK!
-    // ExecuteLuaFile("text_writing_anim.lua");                 // OK!
-    // ExecuteLuaFile("text_ttf_loading.lua");                  // OK!
-    // ExecuteLuaFile("text_bmfont_unordered.lua");             // OK!
+    // [core] module examples
+    // ExecuteLuaFile("core/core_basic_window.lua");
+    // ExecuteLuaFile("core/core_input_keys.lua");
+    // ExecuteLuaFile("core/core_input_mouse.lua");
+    // ExecuteLuaFile("core/core_mouse_wheel.lua");
+    // ExecuteLuaFile("core/core_input_gamepad.lua");
+    // ExecuteLuaFile("core/core_random_values.lua");
+    // ExecuteLuaFile("core/core_color_select.lua");
+    // ExecuteLuaFile("core/core_drop_files.lua");
+    // ExecuteLuaFile("core/core_storage_values.lua");
+    // ExecuteLuaFile("core/core_gestures_detection.lua");
+    // ExecuteLuaFile("core/core_3d_mode.lua");
+    // ExecuteLuaFile("core/core_3d_picking.lua");
+    // ExecuteLuaFile("core/core_3d_camera_free.lua");
+    // ExecuteLuaFile("core/core_3d_camera_first_person.lua");
+    // ExecuteLuaFile("core/core_2d_camera.lua");
+    // ExecuteLuaFile("core/core_world_screen.lua");
+    // ExecuteLuaFile("core/core_vr_simulator.lua");
+    
+    // [shapes] module examples
+    // ExecuteLuaFile("shapes/shapes_logo_raylib.lua");
+    // ExecuteLuaFile("shapes/shapes_basic_shapes.lua");
+    // ExecuteLuaFile("shapes/shapes_colors_palette.lua");
+    // ExecuteLuaFile("shapes/shapes_logo_raylib_anim.lua");
+    // ExecuteLuaFile("shapes/shapes_lines_bezier.lua");
+    
+    // [textures] module examples
+    // ExecuteLuaFile("textures/textures_logo_raylib.lua");
+    // ExecuteLuaFile("textures/textures_image_loading.lua");
+    // ExecuteLuaFile("textures/textures_image_drawing.lua");
+    // ExecuteLuaFile("textures/textures_image_processing.lua");    // ERROR: GetImageData() --> UpdateTexture()
+    // ExecuteLuaFile("textures/textures_rectangle.lua");
+    // ExecuteLuaFile("textures/textures_srcrec_dstrec.lua");
+    // ExecuteLuaFile("textures/textures_to_image.lua");
+    // ExecuteLuaFile("textures/textures_raw_data.lua");            // ERROR: LoadImageEx()
+    // ExecuteLuaFile("textures/textures_particles_blending.lua");
+
+    // [text] module examples
+    // ExecuteLuaFile("text_sprite_fonts.lua");
+    // ExecuteLuaFile("text_bmfont_ttf.lua");
+    // ExecuteLuaFile("text_raylib_fonts.lua");
+    // ExecuteLuaFile("text_format_text.lua");
+    // ExecuteLuaFile("text_writing_anim.lua");
+    // ExecuteLuaFile("text_ttf_loading.lua");
+    // ExecuteLuaFile("text_bmfont_unordered.lua");
     ExecuteLuaFile("text/text_input_box.lua");
-    // ExecuteLuaFile("models_geometric_shapes.lua");           // OK!
-    // ExecuteLuaFile("models_box_collisions.lua");             // OK!
-    // ExecuteLuaFile("models_billboard.lua");                  // OK!
-    // ExecuteLuaFile("models_obj_loading.lua");                // OK!
-    // ExecuteLuaFile("models_heightmap.lua");                  // OK!
-    // ExecuteLuaFile("models_cubicmap.lua");                   // OK!
-    ExecuteLuaFile("models/models_mesh_picking.lua");
-    // ExecuteLuaFile("shaders_model_shader.lua");              // OK!
-    // ExecuteLuaFile("shaders_shapes_textures.lua");           // OK!
-    // ExecuteLuaFile("shaders_custom_uniform.lua");            // OK!
-    // ExecuteLuaFile("shaders_postprocessing.lua");            // OK!
-    // ExecuteLuaFile("audio_sound_loading.lua");               // OK!
-    // ExecuteLuaFile("audio_music_stream.lua");                // OK!
+    
+    // [models] module examples
+    // ExecuteLuaFile("models_geometric_shapes.lua");
+    // ExecuteLuaFile("models_box_collisions.lua");
+    // ExecuteLuaFile("models_billboard.lua");
+    // ExecuteLuaFile("models_obj_loading.lua");
+    // ExecuteLuaFile("models_heightmap.lua");
+    // ExecuteLuaFile("models_cubicmap.lua");
+    // ExecuteLuaFile("models/models_mesh_picking.lua");
+    
+    // [shaders] module examples
+    // ExecuteLuaFile("shaders_model_shader.lua");
+    // ExecuteLuaFile("shaders_shapes_textures.lua");
+    // ExecuteLuaFile("shaders_custom_uniform.lua");
+    // ExecuteLuaFile("shaders_postprocessing.lua");
+    
+    // [audio] module examples
+    // ExecuteLuaFile("audio_sound_loading.lua");
+    // ExecuteLuaFile("audio_music_stream.lua");
     // ExecuteLuaFile("audio_module_playing.lua");
-    // ExecuteLuaFile("audio_raw_stream.lua");                  // ERROR: UpdateAudioStream()
+    // ExecuteLuaFile("audio_raw_stream.lua");      // ERROR: UpdateAudioStream()
+    
+    // [physac] module examples
+    // ExecuteLuaFile("physics_demo.lua");
+    // ExecuteLuaFile("physics_movement.lua");
+    // ExecuteLuaFile("physics_friction.lua");
+    // ExecuteLuaFile("physics_restitution.lua");   // ERROR: UpdateAudioStream()
+    // ExecuteLuaFile("physics_shatter.lua");       // ERROR: UpdateAudioStream()
     
     // De-Initialization
     //--------------------------------------------------------------------------------------
