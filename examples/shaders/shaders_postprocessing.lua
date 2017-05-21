f0476d5d7e46a3954ed5486b3173f3ed9d81d192
raysan5 <raysan5@gmail.com>
2017-05-21 14:52:28 +0200

    Review postprocessing example
    Added some postprocessing shaders

--- examples/shaders/shaders_postprocessing.lua
+++ examples/shaders/shaders_postprocessing.lua
@@ -16,6 +16,39 @@
 --
 -------------------------------------------------------------------------------------------
 
+local MAX_POSTPRO_SHADERS = 12
+
+-- enum PostproShader
+local FX_GRAYSCALE = 1
+local FX_POSTERIZATION = 2
+local FX_DREAM_VISION = 3
+local FX_PIXELIZER = 4
+local FX_CROSS_HATCHING = 5
+local FX_CROSS_STITCHING = 6
+local FX_PREDATOR_VIEW = 7
+local FX_SCANLINES = 8
+local FX_FISHEYE = 9
+local FX_SOBEL = 10
+local FX_BLOOM = 11
+local FX_BLUR = 12
+-- local FX_FXAA = 13
+
+local postproShaderText = {
+    "GRAYSCALE",
+    "POSTERIZATION",
+    "DREAM_VISION",
+    "PIXELIZER",
+    "CROSS_HATCHING",
+    "CROSS_STITCHING",
+    "PREDATOR_VIEW",
+    "SCANLINES",
+    "FISHEYE",
+    "SOBEL",
+    "BLOOM",
+    "BLUR",
+    -- "FXAA"
+}
+
 -- Initialization
 -------------------------------------------------------------------------------------------
 local screenWidth = 800
@@ -23,7 +56,7 @@ local screenHeight = 450
 
 SetConfigFlags(FLAG.MSAA_4X_HINT)      -- Enable Multi Sampling Anti Aliasing 4x (if available)
 
-InitWindow(screenWidth, screenHeight, "raylib [shaders] example - postprocessing shader")
+InitWindow(screenWidth, screenHeight, "raylib [shaders] example - postprocessing shaders")
 
 -- Define the camera to look into our 3d world
 local camera = Camera(Vector3(3.0, 3.0, 3.0), Vector3(0.0, 1.5, 0.0), Vector3(0.0, 1.0, 0.0), 45.0)
@@ -34,8 +67,25 @@ dwarf.material.texDiffuse = texture                                    -- Set dw
 
 local position = Vector3(0.0, 0.0, 0.0)                                -- Set model position
 
-local shader = LoadShader("resources/shaders/glsl330/base.vs", 
-                          "resources/shaders/glsl330/bloom.fs")        -- Load postpro shader
+-- Load all postpro shaders
+-- NOTE 1: All postpro shader use the base vertex shader
+-- NOTE 2: We load the correct shader depending on GLSL version
+local shaders = {}
+
+shaders[FX_GRAYSCALE] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/grayscale.fs")
+shaders[FX_POSTERIZATION] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/posterization.fs")
+shaders[FX_DREAM_VISION] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/dream_vision.fs")
+shaders[FX_PIXELIZER] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/pixelizer.fs")
+shaders[FX_CROSS_HATCHING] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/cross_hatching.fs")
+shaders[FX_CROSS_STITCHING] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/cross_stitching.fs")
+shaders[FX_PREDATOR_VIEW] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/predator.fs")
+shaders[FX_SCANLINES] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/scanlines.fs")
+shaders[FX_FISHEYE] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/fisheye.fs")
+shaders[FX_SOBEL] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/sobel.fs")
+shaders[FX_BLOOM] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/bloom.fs")
+shaders[FX_BLUR] = LoadShader("resources/shaders/glsl330/base.vs", "resources/shaders/glsl330/blur.fs")
+
+local currentShader = FX_GRAYSCALE;
 
 -- Create a RenderTexture2D to be used for render to texture
 local target = LoadRenderTexture(screenWidth, screenHeight)
@@ -51,6 +101,12 @@ while not WindowShouldClose() do            -- Detect window close button or ESC
     -- Update
     ---------------------------------------------------------------------------------------
     camera = UpdateCamera(camera)           -- Update camera
+    
+    if (IsKeyPressed(KEY.RIGHT)) then currentShader = currentShader + 1
+    elseif (IsKeyPressed(KEY.LEFT)) then currentShader = currentShader - 1 end
+    
+    if (currentShader > MAX_POSTPRO_SHADERS) then currentShader = 1
+    elseif (currentShader < 1) then currentShader = MAX_POSTPRO_SHADERS end
     ---------------------------------------------------------------------------------------
 
     -- Draw
@@ -68,29 +124,36 @@ while not WindowShouldClose() do            -- Detect window close button or ESC
                 DrawGrid(10, 1.0)      -- Draw a grid
 
             End3dMode()
-      
-            DrawText("HELLO POSTPROCESSING!", 70, 190, 50, RED)
             
         EndTextureMode()           -- End drawing to texture (now we have a texture available for next passes)
         
-        BeginShaderMode(shader)
+        BeginShaderMode(shaders[currentShader])
         
             -- NOTE: Render texture must be y-flipped due to default OpenGL coordinates (left-bottom)
             DrawTextureRec(target.texture, Rectangle(0, 0, target.texture.width, -target.texture.height), Vector2(0, 0), WHITE)
             
         EndShaderMode()
         
+        DrawRectangle(0, 9, 580, 30, Fade(LIGHTGRAY, 0.7))
+        
         DrawText("(c) Dwarf 3D model by David Moreno", screenWidth - 200, screenHeight - 20, 10, DARKGRAY)
-
-        DrawFPS(10, 10)
-
+        
+        DrawText("CURRENT POSTPRO SHADER:", 10, 15, 20, BLACK)
+        DrawText(postproShaderText[currentShader], 330, 15, 20, RED)
+        DrawText("< >", 540, 10, 30, DARKBLUE)
+        
+        DrawFPS(700, 15)
+            
     EndDrawing()
     ---------------------------------------------------------------------------------------
 end
 
 -- De-Initialization
 -------------------------------------------------------------------------------------------
-UnloadShader(shader)           -- Unload shader
+
+-- Unload all postpro shaders
+for i = 1, MAX_POSTPRO_SHADERS do UnloadShader(shaders[i]) end
+
 UnloadTexture(texture)         -- Unload texture
 UnloadModel(dwarf)             -- Unload model
 UnloadRenderTexture(target)    -- Unload render texture
